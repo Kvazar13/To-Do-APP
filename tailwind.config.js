676ebcfb1212fe079d515893cb330e15d01cf02c
Pavel Kozheykin <pavel_kozheykin@outlook.com>
2023-03-13 14:32:09 +0100

    Feature :: Tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+}
