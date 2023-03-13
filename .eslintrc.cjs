f6e1dce5ba91241c31900428fac60476db7861cc
Pavel Kozheykin <pavel_kozheykin@outlook.com>
2023-03-13 14:20:59 +0100

    Feature :: Set eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,9 +4,9 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 module.exports = {
   root: true,
   'extends': [
-    'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-recommended',
     'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
