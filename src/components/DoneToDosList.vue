5dd513ff9be83667d5cde71df4639c2991387a0d
Pavel Kozheykin <pavel_kozheykin@outlook.com>
2023-03-14 14:54:02 +0100

    Feature :: Done ToDos list styles

--- src/components/DoneToDosList.vue
+++ src/components/DoneToDosList.vue
@@ -1,16 +1,21 @@
 <template>
-  <ul>
-    <li
-      v-for="(doneTodo, index) in doneTodos"
-      :key="index"
-      class="line-through bg-blend-color"
-    >
-      {{ doneTodo }}
-    </li>
-  </ul>
+  <div>
+    <ul>
+      <li
+        v-for="(doneTodo, index) in doneTodos"
+        :key="index"
+        class="flex items-center text-neutral-400 line-through bg-blend-color"
+      >
+        <StatusChecked class="mr-2 h-[20px] w-[20px]" />
+        {{ doneTodo }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script setup>
+import StatusChecked from "@/Icons/StatusChecked.vue";
+
 defineProps({
   doneTodos: {
     type: Array,
