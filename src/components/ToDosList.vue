24daee26bf7863cf50c35688680da29ef16c8be3
Pavel Kozheykin <pavel_kozheykin@outlook.com>
2023-03-14 14:53:49 +0100

    Feature :: ToDos list styles

--- src/components/ToDosList.vue
+++ src/components/ToDosList.vue
@@ -3,14 +3,18 @@
     <li
       v-for="(todo, index) in todos"
       :key="index"
+      class="my-2 flex cursor-pointer items-center rounded-2xl bg-white/80 px-3 py-1"
       @click="$emit('taskDoneEv', index)"
     >
+      <StatusClear class="mr-2 h-[20px] w-[20px]" />
       {{ todo }}
     </li>
   </ul>
 </template>
 
 <script setup>
+import StatusClear from "@/Icons/StatusClear.vue";
+
 defineEmits("taskDoneEv");
 defineProps({
   todos: {
