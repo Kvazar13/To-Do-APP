365b4bce6592e880baaab0d45ea54fdbf3680b67
Pavel Kozheykin <pavel_kozheykin@outlook.com>
2023-03-14 17:17:11 +0100

    Refactor :: Apply new eslint rules

--- src/components/ToDo.vue
+++ src/components/ToDo.vue
@@ -1,9 +1,9 @@
 <template>
   <div
-    class="flex min-h-screen w-full justify-center bg-gradient-to-tr from-red-200 to-indigo-200 py-40"
+      class="flex min-h-screen w-full justify-center bg-gradient-to-tr from-red-200 to-indigo-200 py-40"
   >
     <div
-      class="h-fit min-h-[200px] w-[340px] rounded-2xl bg-white/70 p-5 backdrop-blur"
+        class="h-fit min-h-[200px] w-[340px] rounded-2xl bg-white/70 p-5 backdrop-blur"
     >
       <div class="text-center mb-3 text-2xl text-gray-700">
         Listify
@@ -11,26 +11,26 @@
       <ToDoInput @todo-created="(value) => todos.push(value)"/>
       <div class="mt-3">
         <ToDosList
-          :todos="todos"
-          class="mb-3"
-          @task-done-ev="(index) => taskDone(index)"
+            :todos="todos"
+            class="mb-3"
+            @task-done-ev="(index) => taskDone(index)"
         />
-        <DoneToDosList :done-todos="doneTodos" class="px-3" />
+        <DoneToDosList :done-todos="doneTodos" class="px-3"/>
       </div>
     </div>
   </div>
 </template>
 <script setup>
-import { ref } from "vue";
-import ToDoInput from "@/components/ToDoInput.vue";
-import DoneToDosList from "@/components/DoneToDosList.vue";
-import ToDosList from "@/components/ToDosList.vue";
+import { ref } from 'vue'
+import ToDoInput from '@/components/ToDoInput.vue'
+import DoneToDosList from '@/components/DoneToDosList.vue'
+import ToDosList from '@/components/ToDosList.vue'
 
-const todos = ref([]);
-const doneTodos = ref([]);
+const todos = ref([])
+const doneTodos = ref([])
 
 function taskDone(index) {
-  doneTodos.value.push(todos.value[index]);
-  todos.value.splice(index, 1);
+  doneTodos.value.push(todos.value[index])
+  todos.value.splice(index, 1)
 }
 </script>
