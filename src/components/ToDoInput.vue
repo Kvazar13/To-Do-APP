e3ab912d83da9f3d333c03088b52df9b10506efb
Pavel Kozheykin <pavel_kozheykin@outlook.com>
2023-03-15 08:52:54 +0100

    Refactor :: eslint new rules to ToDoInput.vue

--- src/components/ToDoInput.vue
+++ src/components/ToDoInput.vue
@@ -1,22 +1,22 @@
 <template>
   <form @submit.stop.prevent="submit">
     <input
-      v-model="input"
-      class="focus:shadow-outline w-full appearance-none rounded-2xl border py-2 px-4 leading-tight text-gray-700 shadow focus:outline-none"
-      placeholder="What do you need to do?"
-      type="text"
+        v-model="input"
+        class="focus:shadow-outline w-full appearance-none rounded-2xl border py-2 px-4 leading-tight text-gray-700 shadow focus:outline-none"
+        placeholder="What do you need to do?"
+        type="text"
     />
   </form>
 </template>
 <script setup>
-import { ref } from "vue";
+import { ref } from 'vue'
 
-const input = ref("");
-const emit = defineEmits(["todoCreated"]);
+const input = ref('')
+const emit = defineEmits(['todoCreated'])
 
 function submit() {
-  if (!input.value) return true;
-  emit("todoCreated", input.value);
-  input.value = "";
+  if (!input.value) return true
+  emit('todoCreated', input.value)
+  input.value = ''
 }
 </script>
