7e709b902c8d0c591d104e16b679467396a3ff3b
Pavel Kozheykin <pavel_kozheykin@outlook.com>
2023-03-15 08:14:52 +0100

    Feature :: Make task's text smaller

--- src/components/DoneToDosList.vue
+++ src/components/DoneToDosList.vue
@@ -2,26 +2,28 @@
   <div>
     <ul>
       <li
-        v-for="(doneTodo, index) in doneTodos"
-        :key="index"
-        class="flex items-center text-neutral-400 line-through bg-blend-color"
+          v-for="(doneTodo, index) in doneTodos"
+          :key="index"
+          class="flex items-center text-neutral-400 line-through bg-blend-color"
       >
-        <StatusChecked class="mr-2 h-[20px] w-[20px]" />
-        {{ doneTodo }}
+        <StatusChecked class="mr-2 h-[20px] w-[20px]"/>
+        <span class="text-sm">
+          {{ doneTodo }}
+        </span>
       </li>
     </ul>
   </div>
 </template>
 
 <script setup>
-import StatusChecked from "@/Icons/StatusChecked.vue";
+import StatusChecked from '@/Icons/StatusChecked.vue'
 
 defineProps({
   doneTodos: {
     type: Array,
     default() {
-      return [];
+      return []
     },
   },
-});
+})
 </script>
--- src/components/ToDosList.vue
+++ src/components/ToDosList.vue
@@ -1,27 +1,29 @@
 <template>
   <ul>
     <li
-      v-for="(todo, index) in todos"
-      :key="index"
-      class="my-2 flex cursor-pointer items-center rounded-2xl bg-white/80 px-3 py-1"
-      @click="$emit('taskDoneEv', index)"
+        v-for="(todo, index) in todos"
+        :key="index"
+        class="my-2 flex cursor-pointer items-center rounded-2xl bg-white/80 px-3 py-1"
+        @click="$emit('taskDoneEv', index)"
     >
-      <StatusClear class="mr-2 h-[20px] w-[20px]" />
-      {{ todo }}
+      <StatusClear class="mr-2 h-[20px] w-[20px]"/>
+      <span class="text-sm">
+        {{ todo }}
+      </span>
     </li>
   </ul>
 </template>
 
 <script setup>
-import StatusClear from "@/Icons/StatusClear.vue";
+import StatusClear from '@/Icons/StatusClear.vue'
 
-defineEmits("taskDoneEv");
+defineEmits('taskDoneEv')
 defineProps({
   todos: {
     type: Array,
     default() {
-      return [];
+      return []
     },
   },
-});
+})
 </script>
