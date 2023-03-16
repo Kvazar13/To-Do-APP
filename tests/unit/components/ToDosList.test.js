87405064be134f5fb812aa44bd072f68e5c76d16
Pavel Kozheykin <pavel_kozheykin@outlook.com>
2023-03-16 14:18:35 +0100

    Tests :: Task done event

--- tests/unit/components/ToDosList.test.js
+++ tests/unit/components/ToDosList.test.js
@@ -1,5 +1,6 @@
 import ToDosList from "@/components/ToDosList.vue";
 import { render, screen } from "@testing-library/vue";
+import { mount } from "@vue/test-utils";
 
 describe("ToDosList", () => {
   it("tasks are visible", () => {
@@ -12,5 +13,19 @@ describe("ToDosList", () => {
     tasks.forEach((task) => {
       expect(task).toBeInTheDocument();
     });
+  });
+
+  it("task done event", async () => {
+    const component = mount(ToDosList, {
+      props: {
+        todos: ["Task 1", "Task 2", "Task 3"],
+        emits: ["taskDoneEv"],
+      },
+    });
+    let tasks = component.findAll("[data-testid='to-do-task']");
+    for (const task of tasks) {
+      task.trigger("click");
+    }
+    expect(component.emitted().taskDoneEv).toEqual([[0], [1], [2]]);
   });
 });
